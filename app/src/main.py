a6e45ed49e94405ff49cb1272e7b58331c3f9762
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-05-09 15:38:43 +0200

    Replace wxPython library with tkinter

--- app/src/main.py
+++ app/src/main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 import view
-import wx
 
 if __name__ == '__main__':
-    app = wx.App()
-    frame = view.MainFrame()
-    frame.Show()
-    app.MainLoop()
+    window = view.MainWindow()
+    window.mainloop()
+    
--- app/src/view.py
+++ app/src/view.py
@@ -1,38 +1,10 @@
-import wx
+import tkinter as tk
 
-class MainFrame(wx.Frame):
+class MainWindow(tk.Tk):
     def __init__(self):
-        super(MainFrame, self).__init__(None, title = "Catering")
-        
-        panel = wx.Panel(self)
+        super(MainWindow, self).__init__()
+        self.title('Catering app - BD2')
+        self.geometry('600x400')
 
-        text = wx.StaticText(panel, label = 'Hello catering')
-        font = text.GetFont()
-        font.PointSize += 10
-        font = font.Bold()
-        text.SetFont(font)
-
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(text, wx.SizerFlags().Border(wx.TOP | wx.LEFT, 25))
-        panel.SetSizer(sizer)
-
-        self.make_menu_bar()
-
-        
-    def make_menu_bar(self):
-        help_menu = wx.Menu()
-        about_item = help_menu.Append(wx.ID_ABOUT)
-
-        menu_bar = wx.MenuBar()
-        menu_bar.Append(help_menu, '&Help')
-
-        self.SetMenuBar(menu_bar)
-        self.Bind(wx.EVT_MENU, self.OnAbout, about_item)
-
-
-    def OnExit(self, event):
-        self.Close(True)
-
-
-    def OnAbout(self, event):
-        wx.MessageBox('Robocza aplikacja', "BD2", wx.OK | wx.ICON_INFORMATION)
+        self.label = tk.Label(self, text = 'Hello catering')
+        self.label.grid(column = 0, row = 0)
