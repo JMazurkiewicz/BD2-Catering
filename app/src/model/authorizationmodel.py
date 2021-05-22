cdc8852adf3ee1a1ad8753959c3d8aeb6d3181e4
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-05-22 13:59:43 +0200

    Added draft of connection

--- app/src/model/authorizationmodel.py
+++ app/src/model/authorizationmodel.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # @author: Jakub Mazurkiewicz
 
+import pyodbc
+
 class AuthorizationModel:
     def __init__(self):
         self.login = ''
         self.password = ''
-        self.user_type = -1 # todo: add user types
+        self.user_type = -1
+
+        self.server = 'bd2-grupa11.database.windows.net'
+        self.database = 'BD2'
+        self.driver = '{SQL Server}'
+
 
     def set_login(self, login):
         self.login = login
@@ -16,9 +23,22 @@ class AuthorizationModel:
 
 
     def authorize(self):
-        # do some password verification
         print('password verification started')
-        return True
+        conn = 'DRIVER={};SERVER={};PORT=1433;DATABASE={};UID={};PWD={}'.format(self.driver, self.server, self.database, self.login, self.password)
+        
+        try:
+            with pyodbc.connect(conn) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute('select * from test')
+                    row = cursor.fetchone()
+                    while row:
+                        print('{} + {}', row[0], row[1])
+                        row = cursor.fetchone()
+
+            return True
+        except Exception:
+            print('fatal error')
+            raise
 
 
     def get_user_type(self):
