f0a214b7920d75069061d05cb97afc123ddb5d18
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-06-07 20:47:54 +0200

    Added some allergens

---
+++ app/tests/insert_allergen.sql
@@ -0,0 +1,7 @@
+INSERT INTO allergen (name) VALUE ('gluten');
+INSERT INTO allergen (name) VALUE ('jaja');
+INSERT INTO allergen (name) VALUE ('ryby');
+INSERT INTO allergen (name) VALUE ('orzechy');
+INSERT INTO allergen (name) VALUE ('soja');
+INSERT INTO allergen (name) VALUE ('mleko');
+INSERT INTO allergen (name) VALUE ('skorupiaki');
