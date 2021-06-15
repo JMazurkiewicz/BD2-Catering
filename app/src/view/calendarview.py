da3ad57430234db69630888e26057799ab18b640
Konrad Wojewódzki <01143627@pw.edu.pl>
2021-06-15 17:10:45 +0200

    added some event types

--- app/src/view/calendarview.py
+++ app/src/view/calendarview.py
@@ -15,14 +15,20 @@ class CalendarView(View):
 
         self.calendar = Calendar(parent, font="Comic_Sans 14", selectmode='day', locale='en_US',
                    mindate=self.mindate, disabledforeground='red',
-                   cursor="hand2",width = 300, height = 400)
+                   cursor="hand2")
 
-        self.calendar.tag_config('reminder', background = 'red', foreground = 'blue')
+        self.calendar.tag_config('komunia', background = 'yellow', foreground = 'black')
+        self.calendar.tag_config('wesele', background = 'red', foreground = 'black')
+        self.calendar.tag_config('spotkanie_biznesowe', background = 'blue', foreground = 'white')
+        self.calendar.tag_config('urodziny', background = 'black', foreground = 'yellow')
+
+        #cal.calevent_create(date, 'Hello World', 'message')
+
         self.button_frame = tk.Frame(parent)
         self.__build_grid()
     
     def __build_grid(self):
         print('CalendarView::__build_grid')
-        self.calendar.grid(column=0, row=0, columnspan = 7)
+        self.calendar.grid(column=0, row=0)
         self.button_frame.grid(column=0, row=1)
         
