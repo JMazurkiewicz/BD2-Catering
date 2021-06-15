40b2e25beca1ec62627a6e11d8a9719e6324f97f
Damian Piotrowski <damianp.150898@gmail.com>
2021-06-15 14:17:59 +0200

    added form views

--- app/src/view/mainwindow.py
+++ app/src/view/mainwindow.py
@@ -7,6 +7,8 @@ from os import path
 from model import AuthorizationModel
 from view.authorizationview import AuthorizationView
 from view.newmealview import NewMealView
+from view.neworderview import NewOrderView
+from view.newproductview import NewProductView
 
 class MainWindow(tk.Tk):
     def __init__(self):
@@ -19,6 +21,8 @@ class MainWindow(tk.Tk):
         #self.authorization_model = AuthorizationModel()
         #self.authorization_view = AuthorizationView(self)
         self.newmeal_view = NewMealView(self)
+        #self.neworder_view = NewOrderView(self)
+        #self.newproduct_view = NewProductView(self)
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
         #self.autorization_view.set_model(self.authorization_model)
--- app/src/view/newproductview.py
+++ app/src/view/newproductview.py
@@ -4,28 +4,19 @@
 import tkinter as tk
 from view.formview import FormView
 
-class NewMealView(FormView):
+class NewProductView(FormView):
 
     def __init__(self, parent):
         FormView.__init__(self, parent)
 
+        self.add_entry('catalog_number').set_description('Catalog Number')
         self.add_entry('name').set_description('Name')
-        self.add_entry('weight').set_description('Weight')
-        self.add_entry('ingridient').set_description('Ingridients')
+        self.add_entry('price').set_description('Price')
 
         self.save_button = tk.Button(self, text='Save')
 
         self.__build_grid()
-        self.__build_commands
 
     def __build_grid(self):
         self.entry_frame.grid(column=0, row=0)
         self.save_button.grid(column=0, row=1)
-
-    def __build_commands(self):
-        self.save_button.configure(command=self.on_button_click)
-
-    def on_button_click(self):
-        return None
-
-        
