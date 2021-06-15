9247d3b5cc949e6f4de079fe51540473c99b8343
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-06-15 20:11:32 +0200

    Added main menu buttons

--- app/src/view/controlpanelview.py
+++ app/src/view/controlpanelview.py
@@ -9,17 +9,54 @@ class ControlPanelView(View):
         View.__init__(self, parent, controller)
 
         self.button_frame = tk.Frame(self)
+        self.button_frame.grid(row=0, column=0)
+
         self.buttons = []
         self.__build_buttons()
+        self.grid_columnconfigure(0, weight=1)
 
     
     def __build_buttons(self):
-        self.__add_button(text='Show orders')
-        self.__add_button(text='New order')
-
-    
-    def __add_button(self, *args, **kwargs):
-        button = tk.Button(self.button_frame, *args, **kwargs);
-        column_index = len(self.buttons)
-        button.grid(row=0, column=column_index)
+        button = tk.Button(self.button_frame, text='Products')
+        button.grid(row=0, column=0)
         self.buttons.append(button)
+
+
+        button = tk.Button(self.button_frame, text='Vehicles')
+        button.grid(row=0, column=1)
+        self.buttons.append(button)
+
+
+        button = tk.Button(self.button_frame, text='Magazine')
+        button.grid(row=0, column=2)
+        self.buttons.append(button)
+
+
+        button = tk.Button(self.button_frame, text='Menu')
+        button.grid(row=0, column=3)
+        self.buttons.append(button)
+
+
+        button = tk.Button(self.button_frame, text='Edit employees')
+        button.grid(row=1, column=0)
+        self.buttons.append(button)
+
+
+        button = tk.Button(self.button_frame, text='Add employee')
+        button.grid(row=1, column=1)
+        self.buttons.append(button)
+
+
+        button = tk.Button(self.button_frame, text='Order calendar')
+        button.grid(row=1, column=2)
+        self.buttons.append(button)
+
+
+        button = tk.Button(self.button_frame, text='Employee calendar')
+        button.grid(row=1, column=3)
+        self.buttons.append(button)
+
+
+    def __add_button(self, text, command):
+        button = tk.Button(self.button_frame, text=text, command=command)
+        
