9609cd5e1bfea8d89162651bb9afa8f81ff18c0d
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-06-16 13:28:24 +0200

    Added actions to magazine view

--- app/src/view/magazineview.py
+++ app/src/view/magazineview.py
@@ -14,10 +14,18 @@ class MagazineView(InventoryView):
     def __build_buttons(self):
         button = tk.Button(self.button_frame, text='Add product to magazine')
         button.grid(row=0, column=0)
-        button.configure(command=None) # TODO
+        button.configure(command=self.on_add_product_to_magazine_button_click)
         self.buttons.append(button)
 
         button = tk.Button(self.button_frame, text='Remove product from magazine')
         button.grid(row=1, column=0)
-        button.configure(command=None) # TODO
+        button.configure(command=self.on_remove_product_from_magazine_button_click)
         self.buttons.append(button)
+
+
+    def on_add_product_to_magazine_button_click(self):
+        print('on_add_product_to_magazine_button_click')
+
+
+    def on_remove_product_from_magazine_button_click(self):
+        print('on_remove_product_from_magazine_button_click')
