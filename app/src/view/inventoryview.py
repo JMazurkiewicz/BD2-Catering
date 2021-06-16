efb96a1a74ed69dba3272ee94b0bb35823bdf177
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-06-16 13:13:23 +0200

    Added inventory view base

---
+++ app/src/view/inventoryview.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# @author Jakub Mazurkiewicz
+
+import tkinter as tk
+from view import View
+
+class InventoryView(View):
+    def __init__(self, parent, controller):
+        View.__init__(self, parent, controller)
+
+        self.scroll_frame = tk.Scrollbar(self)
+        self.description = tk.Label(self)
+
+        self.button_frame = tk.Frame(self)
+        self.buttons = []
+        self.__build_grid()
+        
+
+    def __build_grid(self):
+        self.description.grid(row=0, column=0)
+        self.description.grid(row=0, column=1)
