b643940309cba2135edf8a599d2772de93a93e5e
Damian Piotrowski <damianp.150898@gmail.com>
2021-06-17 17:06:39 +0200

    Order 66
    Co-authored-by: kwojewodzki <kwojewodzki@users.noreply.github.com>

--- app/src/model/addclientmodel.py
+++ app/src/model/addclientmodel.py
@@ -8,16 +8,52 @@ class NewClientModel(Model):
         Model.__init__(self)
 
   
-    def add_person(self, costs, cause):
-        sql = 'INSERT INTO client VALUES (\'P\', )'
-        get_id = 'SELECT order_id FROM "Order" WHERE start_date = GETDATE()'
+    def add_person(self, name, surname, phone_nbumber, email, postal_code, street_name, building_number, apartment_number, city, district):
+        #try:
+        address_id = self.add_address(postal_code, street_name, building_number, apartment_number, city, district)
 
-        order_id = self.execute_sql(get_id.format())      
-        self.execute_sql(sql.format(costs, cause, order_id))
+        sql3 = 'INSERT INTO client VALUES (\'P\', {} )'
+        self.execute_sql(sql3.format(address_id.address_id )).commit()
 
-    def add_business(self):
-        return
+        get_client = 'SELECT client_id FROM client WHERE address_id = {}'
+        client_id_cur = self.execute_sql(get_client.format(address_id.address_id))
+        client_id = client_id_cur.fetchone()
+        print(client_id.client_id)
 
-    def get_address(self, city, street, building):
-        sql = 'SELECT a.address_id FROM address AS a JOIN city AS c ON (a.city_id = c.city_id) WHERE c.name = \'{}\' AND a.street_name = \'{}\' AND a.building_number = \'{}\''
-        return self.execute_sql(sql.format(city, street, building))
+        sql4 = 'INSERT INTO person VALUES ({}, \'{}\', \'{}\', \'{}\', \'{}\')'
+        self.execute_sql(sql4.format(client_id.client_id, name, surname, phone_nbumber,email)).commit()
+        print('Person added!')
+
+
+    def add_business(self, postal_code, street_name, building_number, apartment_number, city, district, nip):
+        address_id = self.add_address(postal_code, street_name, building_number, apartment_number, city, district)
+        sql3 = 'INSERT INTO client VALUES (\'B\', {} )'
+        self.execute_sql(sql3.format(address_id.address_id )).commit()
+
+        get_client = 'SELECT client_id FROM client WHERE address_id = {}'
+        client_id_cur = self.execute_sql(get_client.format(address_id.address_id))
+        client_id = client_id_cur.fetchone()
+
+        sql4 = 'INSERT INTO business VALUES ({}, \'{}\')'
+        self.execute_sql(sql4.format(client_id.client_id, nip)).commit()
+        print('Business added!')
+
+
+    def add_address(self, postal_code, street_name, building_number, apartment_number, city, district):
+        sql1 = 'INSERT INTO city VALUES (\'{}\',\'{}\')'
+        self.execute_sql(sql1.format(city, district)).commit()
+
+        get_city = """SELECT city_id
+                          FROM city
+                          WHERE name = \'{}\' """
+        city_id_cur = self.execute_sql(get_city.format(city))
+        city_id = city_id_cur.fetchone()
+
+        sql2 = 'INSERT INTO address VALUES (\'{}\', \'{}\', {}, {}, {})'
+        self.execute_sql(sql2.format(postal_code, street_name, building_number, apartment_number, city_id.city_id)).commit()
+
+        get_address = """SELECT address_id
+                          FROM address
+                          WHERE postal_code = \'{}\' AND street_name = \'{}\' AND building_number = \'{}\' """
+        address_id_cur = self.execute_sql(get_address.format(postal_code, street_name, building_number))
+        return address_id_cur.fetchone()
