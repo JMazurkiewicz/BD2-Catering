9cca201feef8b1b9cd7adb7e52e12a147f89a3ce
Damian Piotrowski <damianp.150898@gmail.com>
2021-06-17 13:53:16 +0200

    employee fix

--- app/src/model/newemployeemodel.py
+++ app/src/model/newemployeemodel.py
@@ -10,32 +10,32 @@ class NewEmployeeModel(Model):
         Model.__init__(self)
   
     def insert_new_employee(self, name, surname, pesel, phone_nbumber, bank_acc_number, postal_code, street_name, building_number, apartment_number, city, district):
-        try:
-            sql1 = 'INSERT INTO address VALUES ({},{})'
-            self.execute_sql(sql1.format(city, district))
+        #try:
+        sql1 = 'INSERT INTO city VALUES (\'{}\',\'{}\')'
+        self.execute_sql(sql1.format(city, district))
 
-            get_city = """SELECT city_id
+        get_city = """SELECT city_id
                           FROM city
                           WHERE name = \'{}\' """
-            city_id_cur = self.execute_sql(get_city.format(city))
-            city_id = city_id_cur.fetchone()
+        city_id_cur = self.execute_sql(get_city.format(city))
+        city_id = city_id_cur.fetchone()
 
-            sql2 = 'INSERT INTO address VALUES ({},{},{}, {})'
-            self.execute_sql(sql2.format(postal_code, street_name, building_number, apartment_number, city_id))
+        sql2 = 'INSERT INTO address VALUES (\'{}\', \'{}\', {}, {})'
+        self.execute_sql(sql2.format(postal_code, street_name, building_number, apartment_number, city_id.city_id))
 
-            get_address = """SELECT address_id
+        get_address = """SELECT address_id
                           FROM address
                           WHERE postal_code = \'{}\' AND street_name = \'{}\' AND building_number = \'{}\' """
-            address_id_cur = self.execute_sql(get_address.format(postal_code, street_name, building_number))
-            address_id = address_id_cur.fetchone()
+        address_id_cur = self.execute_sql(get_address.format(postal_code, street_name, building_number))
+        address_id = address_id_cur.fetchone()
 
-            sql3 = 'INSERT INTO employee VALUES ({},{},{},{},{},{},1)'
-            self.execute_sql(sql3.format(name, surname, pesel, phone_nbumber, bank_acc_number, address_id))
-            print('Employee added!')
+        sql3 = 'INSERT INTO employee VALUES (\'{}\', \'{}\', {}, {}, {}, {}, 1)'
+        self.execute_sql(sql3.format(name, surname, pesel, phone_nbumber, bank_acc_number, address_id.address_id))
+        print('Employee added!')
 
-        except Exception as e:
-            print('While adding employee error occured'.format(e))
-            return
+        #except Exception as e:
+            #print('While adding employee error occured'.format(e))
+            #return
 
         
 
